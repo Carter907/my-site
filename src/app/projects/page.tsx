c926ea8cf77d990dc41eabc2daf0e3c4e5c18918
Carter <speers.carter@gmail.com>
2024-06-28 20:54:03 -0400

    First Commit Next.js journey begins

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,27 @@
+import * as gitty from "@carte_907/gitty-api"
+import getAllRepos from "@carte_907/gitty-api";
+import Project from "./project";
+
+export default async function Projects() {
+  const repos = (await getAllRepos('Carter907')).filter((value, _index, _arr) => {
+
+    if (value.description)
+      return value
+      
+    
+  }).map((repo: gitty.Project) => {
+    return <>
+      <Project params={{ key: repo.name, proj: repo }} />
+    </>
+  });
+
+  return <section className="flex flex-col items-center">
+    <p className="text-4xl">Carter's Projects</p>
+    <p>
+      Hello, Welcome to my Projects.
+    </p>
+    <ul className="w-fit max-h-[500px] overflow-auto">
+      {repos}
+    </ul>
+  </section>
+}
---
+++ src/app/projects/project.tsx
@@ -0,0 +1,14 @@
+import * as gitty from '@carte_907/gitty-api'
+import { FaLink } from 'react-icons/fa'
+
+export default function Project({ params }: { params: { proj: gitty.Project, key: string } }) {
+
+  
+  return <div className="max-w-md mx-auto my-10 bg-white shadow-lg rounded-lg overflow-hidden">
+    <div className="p-6">
+      <h2 className="text-xl font-semibold text-gray-800">{params.proj.name}</h2>
+      <p className="mt-2 text-gray-600"> {params.proj.description}</p>
+      <a href={params.proj.url}><FaLink className="inline"/> to repo...</a>
+    </div>
+  </div>
+}
